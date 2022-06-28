9c230687e94aa83ab2b2652bfc34b29369fd64f5
Matheus <matheus_cs.nf@hotmail.com>
2022-06-28 17:23:53 -0300

    exercicio2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import './App.css';
 import Login from './Pages/Login';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={ Login } />
-        </Switch>
-      </div>
+
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+
     );
   }
 }
