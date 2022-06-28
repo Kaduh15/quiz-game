a397ac667b705b9b9632265db226654e8afdd586
Lucas <lucaslopes@gea.inatel.br>
2022-06-28 15:53:36 -0300

    parcial requisito 1

---
+++ src/Pages/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../trivia.png';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>SUA VEZ</p>
+
+      </header>
+    );
+  }
+}
+
+export default Login;
---
+++ src/Redux/Reducers/login.js
@@ -0,0 +1,15 @@
+const initialState = {
+  name: '',
+  assertions: 0,
+  score: 0,
+  gravatarEmail: '',
+};
+
+const loginReducer = (state = initialState, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default loginReducer;
