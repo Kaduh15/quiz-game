74baab3d7159248b82301847e979cc89925f6483
Matheus <matheus_cs.nf@hotmail.com>
2022-06-29 15:36:05 -0300

    exercicio 19

--- src/Redux/Actions/index.js
+++ src/Redux/Actions/index.js
@@ -5,12 +5,3 @@ export const RESET_SCORE = 'RESET_SCORE';
 export const addNameEmail = (payload) => ({ type: ADD_NAME_EMAIL, payload });
 export const incrementScore = (payload) => ({ type: INCREMENT_SCORE, payload });
 export const resetScore = () => ({ type: RESET_SCORE });
-
-// export const TOKEN = 'ACTIONTOKEN';
-
-// export const actionToken = (req) => ({ type: TOKEN, payload: req });
-
-// export const actionThunkToken = () => (dispatch) => {
-//   const url = 'https://opentdb.com/api_token.php?command=request';
-//   const
-// };
