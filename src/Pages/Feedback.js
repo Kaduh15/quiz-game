3e229e31c60f0d0e45907b7c49fb79f1d380c350
Lucas <lucaslopes@gea.inatel.br>
2022-06-29 13:53:32 -0300

    req 11,12

---
+++ src/Pages/Feedback.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <h1 data-testid="feedback-text">
+          Feedback
+        </h1>
+      </>
+
+    );
+  }
+}
+
+export default Feedback;
