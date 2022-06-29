3a18862ee07aec7b915605d9fca7001ffaca9433
kaduh15 <kadu.silva2014@gmail.com>
2022-06-28 22:31:34 -0300

    requesito 6

--- src/Redux/Actions/index.js
+++ src/Redux/Actions/index.js
@@ -1,6 +1,8 @@
 export const ADD_NAME_EMAIL = 'ADD_NAME_EMAIL';
+export const INCREMENT_SCORE = 'INCREMENT_SCORE';
 
 export const addNameEmail = (payload) => ({ type: ADD_NAME_EMAIL, payload });
+export const incrementScore = (payload) => ({ type: INCREMENT_SCORE, payload });
 
 // export const TOKEN = 'ACTIONTOKEN';
 
